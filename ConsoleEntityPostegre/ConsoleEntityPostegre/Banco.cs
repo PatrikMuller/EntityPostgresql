18324804772c80b0a6bc6a35a9ab47965a832a36
Patrik Muller <patrik.muller@hotmail.com>
2018-08-17 13:17:04 -0400

    Commit Inicial

---
+++ ConsoleEntityPostegre/ConsoleEntityPostegre/Banco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEntityPostegre
+{
+    public class Banco : DbContext
+    {
+        public Banco() : base("entity")
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+    }
+
+}
---
+++ ConsoleEntityPostegre/ConsoleEntityPostegre/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEntityPostegre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Banco db = new Banco();
+            db.Produtos.ToList()
+                .ForEach(p => Console.WriteLine(p.Nome));
+
+
+        }
+    }
+}
